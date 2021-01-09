44933789577e0c668c993557dd899bc3b1a7d0b5
sgalal <sgalal.me@outlook.com>
2021-01-09 16:59:16 +0800

    Translate README to English

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 # qieyun-encoder [![](https://badge.fury.io/py/qieyun-encoder.svg)](https://pypi.org/project/qieyun-encoder/) ![](https://github.com/nk2028/qieyun-encoder/workflows/Python%20package/badge.svg)
 
-《切韻》音系音韻地位編碼器
+Convert a pronunciation of the Qieyun phonological system to a three-character phonological encoding.
 
 Note: this package is only intended for developers.
-
-## Introduction
-
-將一個《切韻》音系音韻地位轉換為三個字符的編碼。
 
 ## Install
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'src/QieyunEncoder/version.py'), encoding='utf8') as f
 setup(
     name='qieyun-encoder',
     version=__version__,
-    description='《切韻》音系音韻地位編碼器',
+    description='Convert a pronunciation of the Qieyun phonological system to a three-character phonological encoding',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/nk2028/qieyun-encoder',
