8ca7cc5bef076828fd1a56d8ca152b405d8065c8
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-05-10 12:02:30 +0800

    Bump version to 0.4.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf8') as f:
 
 setup(
     name='qieyun-encoder',
-    version='0.4.0',
+    version='0.4.1',
     description='A Python library for the operating the basic structure of the Qieyun phonological system',
     long_description=long_description,
     long_description_content_type='text/markdown',
--- src/QieyunEncoder/__init__.py
+++ src/QieyunEncoder/__init__.py
@@ -4,4 +4,4 @@ from .常量 import 常量
 from .音韻地位 import 音韻地位
 from .反切 import 反切
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
