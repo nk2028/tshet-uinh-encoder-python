331ad91b2ca526002714b8955329f81824702605
Ayaka <ayx@google.com>
2024-04-18 23:02:20 +0400

    Undo

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf8') as f:
 
 setup(
     name='qieyun-encoder',
-    version='0.5.0',
+    version='0.4.2',
     description='A Python library for the operating the basic structure of the Qieyun phonological system',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -25,15 +25,15 @@ setup(
         'Natural Language :: Chinese (Traditional)',
         'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
     ],
     keywords='middle-chinese historical-linguistics qieyun',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    python_requires='>=3.7, <4',
+    python_requires='>=3.6, <4',
     entry_points={},
     project_urls={
         'Bug Reports': 'https://github.com/nk2028/qieyun-encoder-python/issues',
