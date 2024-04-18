70e891d5f0bdadf19f91a1d08563e2a424935aa0
Ayaka <ayx@google.com>
2024-04-18 23:06:15 +0400

    Undo

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.9'
     ],
     keywords='middle-chinese historical-linguistics qieyun',
     packages=find_packages('src'),
--- src/QieyunEncoder/工具/__init__.py
+++ src/QieyunEncoder/工具/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
-'''
-與切韻音系相關的工具。
-'''
+from . import 常量
+from .音韻地位 import 音韻地位
+from .工具 import 反切, 正則化韻
+
+__version__ = '0.4.3'
